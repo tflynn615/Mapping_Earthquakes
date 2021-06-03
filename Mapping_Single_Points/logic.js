f69316fdb773be6dc5c4646eb8114c6cee91ac19
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-02 23:22:04 -0500

    Updating with circle markers

--- Mapping_Single_Points/logic.js
+++ Mapping_Single_Points/logic.js
@@ -17,9 +17,24 @@ let map = L.map("mapid", {
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
-    id: "mapbox/streets-v11",
+    id: "mapbox/dark-v10",
     accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// L.circle([34.0522, -118.2437], {
+//   radius: 300,
+//   fillColor: 'lightyellow', 
+//   color: 'black'
+// }).addTo(map);
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    fillColor: 'lightyellow', 
+    color: 'black'
+  }).addTo(map);
