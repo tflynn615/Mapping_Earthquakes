c0618aacb8cf55e701af5af6991900752a0c54f6
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-05 16:06:32 -0500

    Adding layer option

--- Mapping_GeoJSON_Points/logic.js
+++ Mapping_GeoJSON_Points/logic.js
@@ -1,9 +1,6 @@
 // Add console log to confirm its working 
 console.log("working"); 
 
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
-
 // Create a tile layer 
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -12,8 +9,26 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+  Street: streets, 
+  Dark: dark
+};
+
+// Create the map object with center and zoom level.
+let map = L.map("mapid", {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
 
 // // Add GeoJSON data.
 // let sanFranAirport =
