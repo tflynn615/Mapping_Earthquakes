7e2adff1e9ae122b8244f00c53d0731614fb89b2
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-05 14:33:06 -0500

    confirming branch added

--- Mapping_Multiple_Points/logic.js
+++ Mapping_Multiple_Points/logic.js
@@ -24,58 +24,36 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// //  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-L.circle([34.0522, -118.2437], {
-  radius: 300,
-  fillColor: 'lightyellow', 
-  color: 'black'
-}).addTo(map);
+// L.circle([34.0522, -118.2437], {
+//   radius: 300,
+//   fillColor: 'lightyellow', 
+//   color: 'black'
+// }).addTo(map);
 
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    fillColor: 'lightyellow', 
-    color: 'black'
-  }).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     fillColor: 'lightyellow', 
+//     color: 'black'
+//   }).addTo(map);
 
-// An array containing each city's location, state, and population.
-let cities = [{
-  location: [40.7128, -74.0059],
-  city: "New York City",
-  state: "NY",
-  population: 8398748
-},
-{
-  location: [41.8781, -87.6298],
-  city: "Chicago",
-  state: "IL",
-  population: 2705994
-},
-{
-  location: [29.7604, -95.3698],
-  city: "Houston",
-  state: "TX",
-  population: 2325502
-},
-{
-  location: [34.0522, -118.2437],
-  city: "Los Angeles",
-  state: "CA",
-  population: 3990456
-},
-{
-  location: [33.4484, -112.0740],
-  city: "Phoenix",
-  state: "AZ",
-  population: 1660272
-}
-];
+// Get data from cities.js
+let cityData = cities;
+
 
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
+cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location).addTo(map);
+  L.circleMarker(city.location, {
+    radius: city.population/200000,
+    color: "orange",
+    fillColor: "orange",
+    weight: 4
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
  });
 
  
