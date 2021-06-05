654fe84280746174e103e9f4721c578c646bf382
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-05 17:04:38 -0500

    Final polygon code

--- Mapping_GeoJSON_Polygon/logic.js
+++ Mapping_GeoJSON_Polygon/logic.js
@@ -1,19 +1,7 @@
 // Add console log to confirm its working 
 console.log("working"); 
 
-// Create the map object with a center and zoom level 
-//let map = L.map('mapid').setView([40.7, -94.5], 4); 
-
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
-
-
-// Create a tile layer 
+// Create a tile layer
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
@@ -21,5 +9,42 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+let satellitestreets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    maxZoom: 18,
+    id: "mapbox/satellite-streets-v11",
+    accessToken: API_KEY
+});
+
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satellitestreets
+};
+
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets]
+}); 
+
+L.control.layers(baseMaps).addTo(map);
+
+let torontoHoods = "https://raw.githubusercontent.com/tflynn615/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+let myStyle = {
+  color: "blue", 
+  fillColor: "yellow",
+  weight: 1
+};
+
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      console.log(layer); 
+      layer.bindPopup("<h2> Neighborhood: " + feature.properties.AREA_NAME + "</h2>");
+    }
+  }).addTo(map); 
+});
