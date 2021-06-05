7b250910862249c86b50cfae3370ece6d451e1c6
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-05 15:00:55 -0500

    Adding line map js

--- Mapping_Lines/logic.js
+++ Mapping_Lines/logic.js
@@ -4,20 +4,15 @@ console.log("working");
 // Create the map object with a center and zoom level 
 //let map = L.map('mapid').setView([40.7, -94.5], 4); 
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [
-      40.7, -94.5
-    ],
-    zoom: 4
-  });
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([30.1975, -97.6664], 5);
 
 
 // Create a tile layer 
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
-    id: "mapbox/streets-v11",
+    id: "mapbox/satellite-streets-v11",
     accessToken: API_KEY
 });
 
@@ -26,6 +21,17 @@ streets.addTo(map);
 
 // Coordinates for each point to be used in line 
 let line = [
-  [33.9416, -118.4085],
-  [37.6213, -122.3790]
+  [37.6213, -122.3790],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
 ];
+
+// Create a polyline using coordinates 
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  fillOpacity: 0.5,
+  dashArray: '20, 20', 
+  dashOffset: '20'
+}).addTo(map);
