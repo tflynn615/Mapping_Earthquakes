d0be8a3160fe32279dd4e0db2e088582ac0348a4
tflynn615 <78891722+tflynn615@users.noreply.github.com>
2021-06-06 20:00:21 -0500

    Adding logicStep3

--- Earthquakes_past7days/logicStep3.js
+++ Earthquakes_past7days/logicStep3.js
@@ -34,7 +34,7 @@ function styleInfo(feature) {
     return {
         opacity: 1,
         fillOpacity: 1, 
-        fillColor: "#ffae42",
+        fillColor: getColor(feature.properties.mag),
         color: "#000000",
         radius: getRadius(feature.properties.mag),
         stroke: true, 
@@ -49,6 +49,25 @@ function getRadius(magnitude) {
   return magnitude * 4;
 }
 
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+
 
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   console.log(data);
@@ -59,6 +78,9 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
           console.log(data); 
           return L.circleMarker(latlng);
       },
-      style: styleInfo
+      style: styleInfo, 
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("Magnitude: " + feature.properties.mag + "<br> Location: " + feature.properties.place); 
+      }
   }).addTo(map); 
 })
